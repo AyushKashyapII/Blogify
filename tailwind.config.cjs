55bc2f7c0085ccbb9eda26912b02714961d23d69
AYUSH KASHYAP <ayush@gmail.com>
2025-07-06 00:26:25 +0530

    first astro comiit!!!

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['./src/**/*.{astro,js,ts,jsx,tsx,md}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'ui-sans-serif', 'system-ui'],
+      },
+      colors: {
+        background: '#0d0d11',
+      },
+    },
+  },
+  plugins: [],
+};
